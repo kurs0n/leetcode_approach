671a644085fa64d3996a35ab84af8b1e07488668
Patryk404 <patryk744@op.pl>
2023-09-23 23:12:33 +0200

    fix: more correct solution i hope

--- arrays_and_hashing/group_anagrams.js
+++ arrays_and_hashing/group_anagrams.js
@@ -61,3 +61,64 @@
 // };
 
 // console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
+
+
+var groupAnagrams = function(strs){  
+    const countHashMap  = (temp) =>{
+        const map = {}; 
+        for(const letter of temp){
+            if(map[letter]){ 
+                map[letter]++;
+            }
+            else{
+                map[letter] = 1; 
+            }
+        }
+        return map;
+    };
+
+    const isAnagram = (val1,val2) =>{
+        const map1 = countHashMap(val1);
+        const map2 = countHashMap(val2); 
+        const keys1 = Object.keys(map1);
+        const keys2 = Object.keys(map2); 
+        
+        if(keys1.length == keys2.length){  
+            for(const key of keys1){
+                if(map1[key] != map2[key]){
+                    return false;
+               }         
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    const values = [];
+     
+    const ret_arr = [];
+    
+    for(let i=0; i<strs.length; i++){
+        const temp = [];
+        if(values.indexOf(strs[i])==-1){
+            values.push(strs[i]); 
+            temp.push(strs[i]);
+        }
+        for(let j=i+1; j<strs.length; j++){
+            if((isAnagram(strs[i],strs[j])&&values.indexOf(strs[j])==-1) || (isAnagram(strs[i],strs[j])&&strs[i]===strs[j]
+            // && one more condition in case of ["","",""]
+            )){ 
+                values.push(strs[j]);
+                temp.push(strs[j]);
+            }
+        }
+        if(temp.length){
+            ret_arr.push(temp);
+        }
+    } 
+    return ret_arr;    
+}
+
+console.log(groupAnagrams(["",""])); //interesting case
