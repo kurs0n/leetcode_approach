033f8201691b2a7c70d5a0d1e4dda9082c8526a7
kurs0n <patryk744@op.pl>
2024-12-29 23:16:57 +0100

    feat: generateParentheses

---
+++ stack/generateParentheses.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// left -> (, right -> )
+// 0,0
+// 1.0 -> (    -->  // 1,1 --> ()
+// 1,1 -> (      // 2,1 --> ()(
+// 1,1 -> ((	// 2,2 --> ()()
+// 2,1 -> (()
+// 2,2 -> (())
+
+func backtrack(left int, right int, current string, res *[]string, length int) {
+
+	if len(current) == length*2 { //4
+		*res = append(*res, current)
+		return
+	}
+
+	if left < length {
+		backtrack(left+1, right, current+"(", res, length)
+	}
+
+	if right < left {
+		backtrack(left, right+1, current+")", res, length)
+	}
+}
+
+func generateParenthesis(n int) []string {
+	res := []string{}
+	backtrack(0, 0, "", &res, n)
+
+	return res
+}
+
+func main() {
+	fmt.Println(generateParenthesis(3))
+}
